63aacbcdf7b4b2e937e2b797110c10f3954d153d
Киселев Тимур <kiselev@mzta.ru>
2020-08-04 21:09:53 +0300

    removed method fields

--- src/test/java/ru/netology/stats/CalculationStatisticsTest.java
+++ src/test/java/ru/netology/stats/CalculationStatisticsTest.java
@@ -10,7 +10,7 @@ class CalculationStatisticsTest {
     @Test
     void total() {
         int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        CalculationStatistics calculation = new CalculationStatistics();
+
         int actual = calculation.total(month);
         assertEquals(180, actual);
     }
@@ -18,14 +18,14 @@ class CalculationStatisticsTest {
     @Test
     void testAverageamount() {
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        CalculationStatistics calculation = new CalculationStatistics();
+
         assertEquals(15, calculation.averageamount(months));
     }
 
     @Test
     void maxSumm() {
         int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        CalculationStatistics calculation = new CalculationStatistics();
+
         int actual = calculation.maxSumm(month);
         assertEquals(8, actual);
     }
@@ -33,7 +33,7 @@ class CalculationStatisticsTest {
     @Test
     void minSumm() {
         int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        CalculationStatistics calculation = new CalculationStatistics();
+
         int actual = calculation.minSumm(month);
         assertEquals(9, actual);
     }
@@ -41,7 +41,7 @@ class CalculationStatisticsTest {
     @Test
     void minAverageMounths() {
         int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        CalculationStatistics calculation = new CalculationStatistics();
+
         int actual = calculation.minAverageMounths(month);
         assertEquals(5, actual);
     }
@@ -49,7 +49,7 @@ class CalculationStatisticsTest {
     @Test
     void maxAverageMounths() {
         int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-    CalculationStatistics calculation = new CalculationStatistics();
+
     int actual = calculation.maxAverageMounths(month);
     assertEquals(7, actual);
     }
